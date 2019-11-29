aa0bd8dca64fc9cda9bac506bd8ffc6cc35b0cc6
Your Name <you@example.com>
2019-11-29 01:44:06 +0100

    initial release

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # geth_status_plugin4collectd
-A collectd plugin reporting Ethereum geth basic status
+A collectd plugin reporting Ethereum geth basic status.  
+Tested on RPi 4 with Raspbian.  
+Plugin reports (via IPC) **geth service status** (running/stopped), **number of currently connected peers**(nodes), network and local **latest block number** and - based on these two - **sync percentage**.
+
+## quick start
+- install collectd  
+*raspbian: apt-get install collectd*
+- clone the repository  
+*git clone https://github.com/ethbian/geth_status_plugin4collectd.git*
+- create directory for the plugin  
+*mkdir /usr/local/lib/collectd*
+- copy the geth_status.py file to the plugin directory  
+*cp geth_status.py /usr/local/lib/collectd/*
+- update the collectd config file (raspbian: **/etc/collectd/collectd.conf**) by adding to the end of the file:
+
+```
+LoadPlugin python
+<Plugin python>
+    ModulePath "/usr/local/lib/collectd"
+    Import "geth_status"
+    <Module geth_status>
+        Service "geth"
+        Binary "/usr/local/bin/geth/geth"
+        IPCpath "/mnt/ssd/datadir/geth.ipc"
+    </Module>
+</Plugin>
+```
+
+- restart collectd  
+*raspbian: service collectd restart*
+- check logfile for possible errors  
+*raspbian: service collectd status*  
+*raspbian: tail /var/log/syslog*
+
+- options:  
+**Service** - service name used with *systemctl* / *service* commands for stopping and starting geth binary  
+**Binary** - path to the geth binary  
+**IPCpath** - path to the *geth.ipc* file (usually located in the datadir directory)
+- pull requests are more than welcome if you're fixing something
+
---
+++ geth_status.py
@@ -0,0 +1,113 @@
+"""
+This is a collectd plugin reporting different geth stats.
+For more details see https://github.com/ethbian/geth_status_plugin4collectd
+"""
+
+import signal
+import os
+import subprocess
+
+try:
+    import collectd
+except ImportError:
+    raise ImportError('\n\n cannot find Python module: ' +
+                      'collectd\n try executing: pip install collectd')
+
+SERVICE = 'geth'
+BINARY = '/usr/local/bin/geth'
+IPCPATH = '/mnt/ssd/datadir/geth.ipc'
+
+def init():
+    """
+    This method has been registered as the init callback; this gives the plugin a way to do startup
+    actions.
+    """
+    signal.signal(signal.SIGCHLD, signal.SIG_DFL)
+
+def conf(config):
+    """
+    This method has been registered as the config callback and is used to parse options from
+    given config.
+    """
+    for key_val in config.children:
+        key = key_val.key.lower()
+        val = key_val.values[0]
+        if key == 'service':
+            global SERVICE
+            SERVICE = val
+        elif key == 'binary':
+            global BINARY
+            BINARY = val
+        elif key == 'ipcpath':
+            global IPCPATH
+            IPCPATH = val
+        else:
+            collectd.info('geth_status: Ignoring unknown config key: {}'.format(key))
+
+    collectd.info('geth_status: plugin ready')
+
+def read_geth_stats():
+    """
+    This method has been registered as the read callback and will be called every polling interval
+    to dispatch metrics.
+    """
+    try:
+        geth_service = os.system('systemctl is-active --quiet {}'.format(SERVICE))
+    except Exception as exception:
+        collectd.warning('Error checking geth service status: {}'.format(exception))
+        geth_service = 999
+
+    sync_percent = 0
+    binary = True if os.path.exists(BINARY) else False
+    ipc = True if os.path.exists(IPCPATH) else False
+    if binary and ipc and geth_service == 0:
+        try:
+            peers = subprocess.check_output('{} {}{} {}'.\
+                    format(BINARY, 'attach ipc:', IPCPATH, '--exec net.peerCount'),\
+                    shell=True)
+            current = subprocess.check_output('{} {}{} {}'.\
+                      format(BINARY, 'attach ipc:', IPCPATH, '--exec eth.syncing.currentBlock'),\
+                      shell=True)
+            highest = subprocess.check_output('{} {}{} {}'.\
+                      format(BINARY, 'attach ipc:', IPCPATH, '--exec eth.syncing.highestBlock'),\
+                      shell=True)
+            if highest != 0 and current != 0:
+                sync_percent = float(current)/float(highest)*100
+        except Exception as exception:
+            collectd.warning('Getting geth stats error: {}'.format(exception))
+    else:
+        peers = 0
+        current = 0
+        highest = 0
+
+    collectd.Values(plugin='geth_status',
+                    type_instance='service',
+                    type='gauge',
+                    values=[geth_service]).dispatch()
+
+    collectd.Values(plugin='geth_status',
+                    type_instance='peers',
+                    type='gauge',
+                    values=[peers]).dispatch()
+
+    collectd.Values(plugin='geth_status',
+                    type_instance='current',
+                    type='gauge',
+                    values=[current]).dispatch()
+
+    collectd.Values(plugin='geth_status',
+                    type_instance='highest',
+                    type='gauge',
+                    values=[highest]).dispatch()
+
+    collectd.Values(plugin='geth_status',
+                    type_instance='sync',
+                    type='gauge',
+                    values=[round(sync_percent, 3)]).dispatch()
+
+if __name__ != '__main__':
+    collectd.register_init(init)
+    collectd.register_config(conf)
+    collectd.register_read(read_geth_stats)
+else:
+    raise SystemExit('Nope - it is a plugin reporting to collected.')
